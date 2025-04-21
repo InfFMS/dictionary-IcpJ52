b75c32ff5e38c223bdd4f534cbd30172f9d755f9
IcpJ42 <iblion08@gmail.com>
2025-04-21 21:35:36 +0700

    Сделал 8 задание

--- task8.py
+++ task8.py
@@ -1,3 +1,15 @@
 # Представьте, что вы шпионы)
 # Придумайте шифр, в котором буквы заменяются на какие-то символы
 # и реализуйте шифроватор и дешифроватор
+def coder(string, key):
+    code_str = ''
+    for el in string:
+        code_str += chr(key + ord(el))
+    return code_str
+
+
+def decoder(string, key):
+    decode_str = ''
+    for el in string:
+        decode_str += chr(ord(el) - key)
+    return decode_str
